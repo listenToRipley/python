4985d919b4ce1bda48f3cff2afe898ed71c4fe88
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-07 15:10:01 -0400

    some class options

--- Notes.md
+++ Notes.md
@@ -2,6 +2,8 @@
 
 ## Classes
 
+You can see example of these [here](./classes.py)
+
 Attributes of a class will only be modified by the specific instance.
 
 If you want to see the values included included within a classes, you can use `__dict__` with the instance names prior to it to see the complete object.
@@ -14,4 +16,4 @@ Self is variable references as a specific instance.
 
 Within a classes, own instance are you use the magic method of `__init__`. This is allow these to always be unique to each instance. 
 
-If you fail to include the variables listed in `__init__`, this will require in an error as these are now considered required elements to create an object, unless you provide default options.
+If you fail to include the variables listed in `__init__`, this will require in an error as these are now considered required elements to create an object, unless you provide default options. This is also known as encapsulation. 
---
+++ classes.py
@@ -0,0 +1,47 @@
+class User:
+  def __init__(self, username, email):
+    self.username = username
+    self.email = email
+
+  def info(self):
+    print(f"Use {self.username} has email {self.email}")
+
+first_user = User('user1', 'example@example.com')
+# print(first_user.info())
+
+class Comment: 
+  def __init__(self, text):
+    self.text = text
+    self.votes_qty = 0
+
+  def upvote(self):
+    self.votes_qty +=1
+
+
+# print(Comment.upvote)# upvote here is considered a function at this point
+# # if passed at upvote() then it would get error as it should be an instance
+
+# first_comment = Comment("First comment")
+# print(first_comment.text)
+# print(first_comment.votes_qty)
+# print(first_comment.__dict__)
+# print(first_comment.upvote) # bound method now that the instance has been created
+# first_comment.upvote()
+# #also valid syntax = Comment.upvote(first_comment)
+# print(first_comment.votes_qty)
+
+
+class Post:
+  def __init__(self, title, content, author):
+    self.title = title
+    self.content = content
+    self.author - author
+    self.likes_qty = 0 #default value
+
+  def likes(self):
+    self.likes_qty += 1
+
+
+my_post = Post("First post", "Some info passed here.","Username")
+print(my_post.title)
+print(my_post.author)
