b4f914943366c7f32f5420e349d7a671ef3f2cc1
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-08 20:54:51 -0400

    static methods

--- Notes.md
+++ Notes.md
@@ -17,3 +17,11 @@ Self is variable references as a specific instance.
 Within a classes, own instance are you use the magic method of `__init__`. This is allow these to always be unique to each instance. 
 
 If you fail to include the variables listed in `__init__`, this will require in an error as these are now considered required elements to create an object, unless you provide default options. This is also known as encapsulation. 
+
+### Static Methods
+
+You would use these when you need to create methods that are independent of the specific instances of the class. Which means that you do note have to have a instance created in order to call these methods, they are functions associated with the class elements, but can be create by themselves. Think of it as a general function located inside of a class.
+
+Non static methods are known as bound methods.
+
+These do not require a self parameter and called directly.
--- classes.py
+++ classes.py
@@ -17,11 +17,17 @@ class Comment:
   def upvote(self):
     self.votes_qty +=1
 
+  @staticmethod
+  def merge_comments(first, second):
+    return f"{first}, {second}"
+
 
 print(Comment.upvote)# upvote here is considered a function at this point
 # # if passed at upvote() then it would get error as it should be an instance
 
 first_comment = Comment("First comment")
+m_1 = Comment.merge_comments("Thanks!", "Wonderful")
+m_2 = first_comment.merge_comments("okay", "great!")
 print(first_comment.text)
 print(first_comment.votes_qty)
 print(first_comment.__dict__)
@@ -41,6 +47,13 @@ class Post:
   def likes(self):
     self.likes_qty += 1
 
+  @staticmethod
+  def format_post(title, comment):
+    return (
+      f"Title: {title}\n"
+      f"Comment: {comment}"
+    )
+
 
 my_post = Post('Title', 'Content', 'Author')
 print(my_post.title)
@@ -48,3 +61,26 @@ print(my_post.author)
 my_post.likes()
 my_post.likes()
 print(my_post.likes_qty)
+format_post = Post.format_post("This is the title", "This is the post content.")
+print(format_post)
+
+class Calculator:
+  @staticmethod
+  def add(a,b):
+    return a+b
+  
+  @staticmethod
+  def subtract(a,b):
+    return a-b
+  
+  @staticmethod
+  def mult(a,b):
+    return a*b
+  
+  @staticmethod
+  def divide(a,b):
+    if b!=0:
+      return a/b
+    raise ValueError("Can't divide by zero.")
+
+print(Calculator.add(20,10))
