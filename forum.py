9b9ffdaf2b1c419cf64ba2e527e97e7c1d465a1d
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-16 16:41:12 -0400

    find user

--- forum.py
+++ forum.py
@@ -16,8 +16,35 @@ class Forum:
 
     def register_user(self, username, email):
         user = User(username, email)
-        users.append(user)
+        self.users.append(user)
+        return user
 
     def create_post(self, title, content, author):
         post = Post(title, content, author)
-        posts.append(post)
+        self.posts.append(post)
+        return post
+
+    def find_user_by_username(self, username):
+        for user in self.users:
+            if user.username == username:
+                return user
+
+    # def find_post_by_username():
+
+forum = Forum()
+
+ripley = forum.register_user("listenToRipley", "ripley@wayland.com")
+arthur = forum.register_user("dent.a","adent@gmail.com")
+
+print(forum.users)
+
+forum.create_post("Towels", "Are the best, don't panic.", arthur)
+
+# print(forum.posts)
+# print(forum.posts[0].title)
+# print(forum.posts[0].content)
+# print(forum.posts[0].author.username)
+# print(forum.posts[0].author.email)
+
+print(forum.find_user_by_username('ripley'))
+print(forum.find_user_by_username('dent.a').__dict__)
