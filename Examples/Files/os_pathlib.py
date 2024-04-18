3ceeaf922692445ca22d0a3a8fda983bbf09cda8
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-18 12:27:21 -0400

    files

--- Examples/Files/os_pathlib.py
+++ Examples/Files/os_pathlib.py
@@ -12,3 +12,26 @@ print(Path('.').absolute())
 # ~/Documents/GitHub/python/
 
 print(type(Path))
+
+file_path = Path('test.txt')
+
+print([m for m in dir(file_path)
+       if not m.startswith('_')])
+
+# find path on the *current* directory
+print(Path.cwd())
+
+# create new path
+print(Path('usr').joinpath('local').joinpath('bin'))
+# or
+print(Path('usr') / 'local' / 'bin')
+
+# Checking the *current directory* presence of the directory or file
+print(Path('main.py').exists())
+print(Path('home/nken/Documents').exists())
+print(Path('other.py').exists())
+# these are absolute paths
+
+print(Path('os_pathlib.py').is_file())
+print(Path('../Files').is_file())
+print(Path('../Files').is_dir())
