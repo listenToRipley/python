84a42882e4062172c0d94d8bfbee3466869058e1
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-18 15:25:35 -0400

    file paths

--- Examples/Files/os.py
+++ Examples/Files/os.py
@@ -1,6 +1,29 @@
-from os import path
+# from os import path
+import os
 
-print(path.abspath('.'))
-# ~/Documents/GitHub/python/
+# print(path.abspath('.'))
 
-print(type(path))
+# print(type(path))
+
+directory_path = 'my_test_directory'
+
+if not os.path.exists(directory_path):
+    # create directory
+    os.mkdir(directory_path)
+    print(f"Directory was created: {directory_path}")
+else:
+    print(f"Directory exists: {directory_path}")
+# Create path to file
+file_path = os.path.join(directory_path, 'my_file.txt')
+print(f"File path: {file_path}")
+
+# get parent directory since the relationship is relative
+parent_dir = os.path.dirname(file_path)
+print(f"Parent directory: {parent_dir}")
+
+# checking if file is the file or dir path.
+is_file = os.path.isfile(file_path)
+is_dir  = os.path.isdir(directory_path)
+
+print(f"{file_path} is a file? {is_file}")
+print(f"{directory_path} is a directory? {is_dir}")
