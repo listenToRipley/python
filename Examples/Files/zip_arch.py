d259407848fceac3cb7b86063588293d3444a23c
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-19 16:59:26 -0400

    zip methods

--- Examples/Files/zip_arch.py
+++ Examples/Files/zip_arch.py
@@ -1,9 +1,14 @@
 from zipfile import ZipFile
 from pathlib import Path
 
-my_dir = Path('Examples/Files/ZipTest')
-if not my_dir.exists():
+my_dir = Path('Examples/Files/ZipTest') # directory path
+if not my_dir.exists(): #create directory if it doesn't exist
     my_dir.mkdir()
+
+# remove zip file if it exists
+zip_archive = Path('Examples/Files/zip-test.zip')
+if zip_archive.exists():
+    zip_archive.unlink()
 
 file_one = 'Examples/Files/ZipTest/first.txt'
 file_two = 'Examples/Files/ZipTest/second.txt'
@@ -13,10 +18,15 @@ with open(file_one, 'w') as file:
 
 
 with open(file_two, 'w') as file:
-    file.write("First line in this file\n")
+    file.write("First line in this file\n")  
 
 # Create zip
 with ZipFile('Examples/Files/zip-test.zip', mode='w') as zip_file:
     for file in my_dir.iterdir():
         print(file)
         zip_file.write(file)
+
+# Open zip
+with ZipFile(zip_archive) as zip_file:
+    print(zip_file)
+    print(type(zip_file))
