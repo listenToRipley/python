615828833a427de160d1e0c33deeadc248da83d2
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-22 13:25:39 -0400

    date and time

--- Examples/Modules/date_time.py
+++ Examples/Modules/date_time.py
@@ -1,16 +1,21 @@
-import datetime
+# import datetime
+from datetime import datetime
+# future_date = datetime.date(2222, 9,20)
+# print(f"Future date: {future_date}")
 
-future_date = datetime.date(2222, 9,20)
-print(f"Future date: {future_date}")
+# print(future_date.year)
+# print(future_date.month)
+# print(future_date.day)
 
-print(future_date.year)
-print(future_date.month)
-print(future_date.day)
+# some_time = datetime.time(15,30,10)
 
-some_time = datetime.time(15,30,10)
+# print(f"Some time: {some_time}")
 
-print(f"Some time: {some_time}")
+# print(some_time.hour)
+# print(some_time.minute)
+# print(some_time.second)
 
-print(some_time.hour)
-print(some_time.minute)
-print(some_time.second)
+new_future_datetime = datetime(2100, 10,12,20,55,15)
+print(f"Future date and time {new_future_datetime}")
+
+print(dir(new_future_datetime))
