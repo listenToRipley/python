252d07e35b2850cb77cf32830de1f33711e5900f
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-22 14:46:51 -0400

    password generator

--- Examples/Modules/secerts_mod.py
+++ Examples/Modules/secerts_mod.py
@@ -2,14 +2,24 @@ import secrets
 import string
 
 #string options
-print(string.digits)
-print(string.ascii_letters)
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.punctuation)
+# all_chars = string.ascii_letters + string.digits + string.punctuation
 
-all_chars = string.ascii_letters + string.digits + string.punctuation
-print(all_chars)
+# print(string.digits)
+# print(string.ascii_letters)
+# print(string.ascii_lowercase)
+# print(string.ascii_uppercase)
+# print(string.punctuation)
+# print(all_chars)
+
 # create passwords
 
-print(secrets.choice(all_chars))
+# print(secrets.choice(all_chars)) # this will only provide a single elements.
+
+def generate_password(length):
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(list(secrets.choice(all_chars) for i in range(length)))
+    return password
+
+print(generate_password(3))
+print(generate_password(9))
+print(generate_password(15))
