7446c419e4324c1bfe1ded43306a3d08e46b90e3
listenToRipley <natalie.m.kendrick@gmail.com>
2024-04-22 20:33:34 -0400

    run multiple lines

--- Examples/Files/sqllite.py
+++ Examples/Files/sqllite.py
@@ -20,8 +20,23 @@ DB_NAME = 'sqlite_db.db'
 #   #run request
 #   sqlite_conn.execute(sql_request)
 
-# Write data to table
+# Write a record to table
+# with sqlite3.connect(DB_NAME) as sqlite_conn:
+#   sql_request = " INSERT INTO courses VALUES (?,?,?,?)" # ? are placeholders
+#   sqlite_conn.execute(sql_request, (251, "Intro to Python", 100, 30)) # pass tuples for values
+#   sqlite_conn.commit() # run the requests
+
+
+# add multiple records  to the table.
+courses = [
+  (252, "Intro to JavaScript", 100, 30),
+  (351, "Understanding Algorithms", 50, 25),
+  (773, "Getting to Know Node.js", 75, 50),
+  (101, "How to Rubber Duck", 5, 90)
+]
+
 with sqlite3.connect(DB_NAME) as sqlite_conn:
   sql_request = " INSERT INTO courses VALUES (?,?,?,?)" # ? are placeholders
-  sqlite_conn.execute(sql_request, (251, "Intro to Python", 100, 30)) # pass tuples for values
+  for course in courses:
+    sqlite_conn.execute(sql_request, course)
   sqlite_conn.commit() # run the requests
