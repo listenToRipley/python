4e681dc58848afd03de967d366588820b28341a2
listenToRipley <natalie.m.kendrick@gmail.com>
2024-07-17 12:19:53 -0400

    context to 404

--- Examples/django_proj/shop/views.py
+++ Examples/django_proj/shop/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
+# from django.http import HttpRequest
 from django.http import Http404
 from .models import Course
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -10,8 +11,13 @@ def index(request):
   return render(request, 'courses.html', {'courses': courses})
 
 def single_course(request, course_id):
-  try:
-    course = Course.objects.get(pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
-  except Course.DoesNotExist:
-    raise Http404
+  #OPTION 1
+  # try:
+  #   course = Course.objects.get(pk=course_id)
+  #   return render(request, 'single_course.html', {'course': course})
+  # except Course.DoesNotExist:
+  #   raise Http404
+  
+  # OPTION 2 - give context to error
+  course = get_object_or_404(Course, pk=course_id)
+  return render(request, 'single_course.html', {'course': course})
