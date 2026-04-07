237d13f57086236af7c1f929f2cc1a9295bd897a
listenToRipley <natalie.m.kendrick@gmail.com>
2026-04-07 13:40:33 -0400

    add prompts

--- Zenva/MedicalDiagnosisBot/main.py
+++ Zenva/MedicalDiagnosisBot/main.py
@@ -1,31 +1,80 @@
-# Note that """ allows you to break a string into multiple lines so you can better visualize it
 welcome_prompt = """Welcome doctor, what would you like to do today?\n
 - To list all patients, press 1\n
 - To run a new diagnosis, press 2\n
 - To quit, press q\n"""
 
+name_prompt = "What is the patient's name?\n"
+appearance_prompt = "How is the patient's general appearance?\n - 1: Normal appearance\n - 2: Irritable or lethargic\n"
 
+eye_prompt = "How are the patient's eyes?\n - 1: Eyes normal or slightly sunken\n - 2: Eyes very sunken\n"
+skin_prompt = "How is the patient's skin when you pinch it?\n - 1: Normal skin pinch\n - 2: Slow skin pinch\n"
+
+severe_dehydration = "Severe dehydration"
+some_dehydration = "Some dehydration"
+no_dehydration = "No dehydration"
+
+# Initial list of patients and diagnoses
+patients_and_diagnoses = [
+    "Mike - Severe dehydration",
+    "Sally - No dehydration",
+    "Kate - Some dehydration",
+]
+
+
+# Prints out the list of patients and their diagnoses
 def list_patients():
-    print("Listing patients and diagnoses")
+    for patient in patients_and_diagnoses:
+        print(patient)
+
+
+# Saves a new patient and diagnosis to the list
+def save_new_diagnosis(name, diagnosis):
+    final_diagnosis = name + " - " + diagnosis
+    patients_and_diagnoses.append(final_diagnosis)
+    # Prints out the final diagnosis of the patient in question
+    print("Final diagnosis:", final_diagnosis, "\n")
+
+
+def assess_eyes(eyes):
+    if eyes == "1":
+        return no_dehydration
+    elif eyes == "2":
+        return severe_dehydration
+
+
+def assess_skin(skin):
+    if skin == "1":
+        return some_dehydration
+    elif skin == "2":
+        return severe_dehydration
+
+
+def assess_appearance():
+    appearance = input(appearance_prompt)
+    if appearance == "1":
+        eyes = input(eye_prompt)
+        return assess_eyes(eyes)
+    elif appearance == "2":
+        skin = input(skin_prompt)
+        return assess_skin(skin)
 
 
 def start_new_diagnosis():
-    print("Starting a new diagnosis")
+    name = input(name_prompt)
+    diagnosis = assess_appearance()
+    # Calls the save_new_diagnosis function to update the patients_and_diagnoses list
+    save_new_diagnosis(name, diagnosis)
 
 
 def main():
-    selection = input(welcome_prompt)
-    # Using an if statement, we branch the code according to user selection:
-    if selection == "1":
-        # If 'selection' equals to 1, we're going to call the list_patients function
-        list_patients()
-    # Otherwise, if 'selection' equals to 2, we're going to call the start_new_diagnosis function
-    elif selection == "2":
-        start_new_diagnosis()
-    # And if 'selection' equals to q, we're going to simply quit the program
-    elif selection == "q":
-        return
+    while True:
+        selection = input(welcome_prompt)
+        if selection == "1":
+            list_patients()
+        elif selection == "2":
+            start_new_diagnosis()
+        elif selection == "q":
+            return
 
 
-# Starts the program execution
 main()
--- Zenva/README.md
+++ Zenva/README.md
@@ -2,6 +2,15 @@
 
 [Python Mini-Degree](https://academy.zenva.com/product/python-programming-mini-degree/)
 
+### 4 Stages of Project Management
+
+    - Definition
+    - Design
+    - Implementation
+    - Evaluation
+
 ## Turtle
 
 Moving pixels around screens [examples](./turtle.py)
+
+## Bot
